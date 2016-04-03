9119f808ec211b9664ac6e5aa67ade87208b73b6
Jack Zheng <jack.zheng@stonybrook.edu>
2016-04-02 22:37:49 -0400

    Update compute.php

--- PHP/compute.php
+++ PHP/compute.php
@@ -40,9 +40,24 @@ $client_secret = "3HPKD4GEX3O0H4QFDBK5M4THJSKDZFI24KKXHOKWE1PQAQYY";
 $json = file_get_contents("https://api.foursquare.com/v2/venues/explore
   						  ?client_id=" . $client_id . 
   						  "&client_secret=" . $client_secret . 
-						  "&v=20130815
+						  "&v=20160301
 						  &ll=40.7,-74
 						  &query=" . $most_requested);
 
+$restaurant_info = json_decode($json); 
+
+<!-- Retrieving venue name from json string-->
+$venue = $restaurant_info->response->venues[0]; 
+$name = $venue->name; 
+$number = $venue->contact->formattedPhone; 
+$location;
+<!-- Concatanating address-->
+foreach ($venue->location->formattedAddress as $item) {
+	$location = $location . $item;
+}
+$distance = $venue->location->distance;
+
+
+$venue_info = ($name, $number, $location);
 
 ?>
