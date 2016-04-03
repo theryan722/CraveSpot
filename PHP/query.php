8608aefaa4c3642cdc561da08e254a0b11cf4352
Ryan O'Day <romanguy722@gmail.com>
2016-04-03 06:39:31 -0400

    Fix and update query

--- PHP/query.php
+++ PHP/query.php
@@ -1,51 +1,18 @@
-<!-- To record the queries sent by the users -->
 <?php
-
-//Retrieves number of choices user picked 
-$choices = count($_GET); 
-
-if ($choices > 0) {
-	$keys = array_keys($_GET); 
-	$mydata; 
-
-	//Create an associative array of key value pairs that are not the id 
-	foreach ($keys as $item) {
-		$temp = strtolower($item);
-		if ($temp != 'id') {
-			$mydata[$temp] = strtolower($_GET[$item]); 
-		}
+if (sizeof($_GET) > 0) {
+  $data = $_GET['choice1'] . "|" . $_GET['choice2'] . "|" . $_GET['choice3'] . "|" . $_GET['choice4'] . "|" . $_GET['choice5'];
+	$open = fopen("groups/" . $_GET['id'] . ".txt", "a+");
+  $contains = false;
+  foreach (new SplFileObject("groups/" . $_GET['id'] . ".txt") as $line) {
+    if ($line == $data){
+      $contains = true;
+      break;
+    }
 	}
-
-	//-- Get the id of the new group 
-	$id = $_GET['id'];
-
-
-	//<!-- Name of the file to be written to-->
-	$filename = $id . ".txt";
-
-	//<!-- Replace the templateurl with the real url later-->
-	$abpath = "templateurl/engine/groups/" . $filename; 
-	
-	//<!-- Opens/create a new file with $abpath -->
-	$file = fopen($abpath, "a+");
-
-	//<!-- Append and start writing in selections-->
-
-	while ($key = current($mydata)) {
-		if ($mydata[$key] != "empty") {
-			fwrite($file, $mydata[$key]. "|");
-		}
-		
-	}
-
-	//<!-- not sure if newline is needed -->
-	fwrite($file, "\n");
-	fclose($file);
-
-} else {
-	echo "No arguments were sent\n";
-}
-
+  if (! $contains){
+    fwrite($open, $data);
+    echo "SUBMITTED";
+  }
+	fclose($open);
+} 
 ?>
-
-<!-- sample url:   cravespace.com/query.php?id=12345&choice1 = "pizza"&choice2 = "cake"&choice3 = 5 ... etc -->
