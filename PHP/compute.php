f40c542e71cbc26fe6998b596d973d243f8d7033
Jack Zheng <jack.zheng@stonybrook.edu>
2016-04-03 00:53:43 -0400

    Update compute.php

--- PHP/compute.php
+++ PHP/compute.php
@@ -2,7 +2,7 @@
 <?php
 
 <!-- File url may be changed later -->
-$filename = "templateurl.com/engine/groups/" . $_GET["id"] . ".txt";
+$filename = "/groups/" . $_GET["id"] . ".txt";
 
 $file = fopen($filename, "a+") or die echo "File was not opened\n";
 
