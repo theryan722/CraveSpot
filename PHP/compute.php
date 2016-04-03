f1ad7aa8630fee89801912bafd36e549b8498c31
Jack Zheng <jack.zheng@stonybrook.edu>
2016-04-03 08:33:09 -0400

    Update compute.php

--- PHP/compute.php
+++ PHP/compute.php
@@ -9,32 +9,34 @@ if (! $file == false) {
 	foreach (new SplFileObject($filename) as $line) {
 		$str = str_replace("\n", '', $line);
 		$array = explode("|", $str);
-  	foreach ($array as $ind){
-    	if (isset($total[$ind])) {
-    		$total[$ind]++; 
-    	} else {
-    		$total[$ind] = 1;
-    	}
-  	}
+	  	foreach ($array as $ind){
+		    	if (isset($total[$ind])) {
+		    		$total[$ind]++; 
+		    	} else {
+		    		$total[$ind] = 1;
+		    	}
+	  	}
 	}
-  fclose($file);
+  	fclose($file);
 	$high = 0; 
 	foreach ($total as $key => $value) {
-  	if ($value > $high) {
-    	$high = $value; 
-    	$keys = $key;
-  	}
+	  	if ($value > $high) {
+	    	$high = $value; 
+	    	$keys = $key;
+	  	}
 	}
-  $bool = true;
+  	$bool = true;
 	}	else{
- 		echo "Group does not exist"; 
-}
+ 			echo "Group does not exist"; 
+	}
 if ($bool) {
   $client_id = "MZBXZQHK3HDVZXRJQN3GLZQ3FI5W2XNRUEYMUZIFYKQWUVN0"; 
   $client_secret = "3HPKD4GEX3O0H4QFDBK5M4THJSKDZFI24KKXHOKWE1PQAQYY";
   $json = file_get_contents("https://api.foursquare.com/v2/venues/explore?client_id=" . $client_id . "&client_secret=" . $client_secret . "&v=20160301&ll=40.7286679,-73.9956593&query=" . $keys);
-	$js = json_decode($json,true);
-  $resp = $js['response']['groups'][0]['items'][0]['venue']['name'] . "|" . $js['response']['groups'][0]['items'][0]['venue']['location']['formattedAddress'][0] . $js['response']['groups'][0]['items'][0]['venue']['location']['formattedAddress'][1];
+  $js = json_decode($json,true);
+  $query = $js['response']['groups'][0]['items'][0]['venue'];
+  //$resp = $js['response']['groups'][0]['items'][0]['venue']['name'] . "|" . $js['response']['groups'][0]['items'][0]['venue']['location']['formattedAddress'][0] . $js['response']['groups'][0]['items'][0]['venue']['location']['formattedAddress'][1];
+  $resp = $query['name'] . "|" . $query['location']['formattedAddress'][0] . $query['location']['formattedAddress'][1]
   $fp = fopen("groups/" . $_GET['id'] . ".response",  "w"); 
   fwrite($fp, $resp);
   echo "COMPUTED";
